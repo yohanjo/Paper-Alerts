d62999b101af061fd847965fcd7fd232d1b16f8f
yohan <yohan@radio.pc.cs.cmu.edu>
2017-06-25 19:59:56 -0400

    Added run scripts.

---
+++ src/main/run_acm_proceedings_alert.py
@@ -0,0 +1,12 @@
+'''
+Run the alert system for WikiCFP.
+
+@author: Yohan
+@version: June 21, 2017
+'''
+import os
+
+email_address = "your@email.address"
+alert_interval = 24
+
+os.system("python wikicfp_alert.py %s %d" % (email_address, alert_interval))
